3b50b691dc52f60b304d1dc59063ce260dcaaf7c
Stefano Straga <stefanostraga2k18@gmail.com>
2022-05-30 17:05:58 -0300

    subiendo el programa finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Si el importe es menor a ARS 1000, no hay descuento.
+            // Si el importe es ARS 1000 o más pero menor a ARS 5000, aplica un descuento del 10%.
+            // Si el importe es ARS 5000 o más, aplica un descuento del 18%.
+
+            float importe;
+
+            Console.WriteLine("Ingrese el importe");
+            importe = float.Parse(Console.ReadLine());
+
+            if (importe >= 1000){
+                if(importe > 5000) {
+                    importe = importe * 0.82F;
+                } else {
+                    importe = importe * 0.90F;
+                }
+            } 
+
+            Console.WriteLine("El importe a cobrar es: " + importe);
+        }
+    }
+}
